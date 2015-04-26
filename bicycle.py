0c16f4fcff26dab0730db23a00808c584a15674e
David Chu <dchu101@thinkful.com>
2015-04-26 00:06:34 +0000

    Second commit

---
+++ bicycle.py
@@ -0,0 +1,29 @@
+class Bicycle(object):
+    def __init__(self,nbikeame):
+        self.bikename = bikename
+      
+    def __init__(self,weight):
+        self.weight = weight
+      
+    def __init__(self, cost):
+        self.cost = cost
+
+class BikeShops(object):
+    def __init__(self,shopname):
+        self.shopname = shopname
+        
+    def __init__(self,inventory):
+        
+#Have an inventory of different bicycles
+#Sell bicycles with a margin over their cost
+#Can see how much profit they have made from selling bikes
+
+class Customers(object):
+    def __init__(self,custname):
+        self.custname = custname
+    
+    def __init__(self,funds):
+        self.funds = funds
+        
+    # Can buy and own a new bicycle
+
