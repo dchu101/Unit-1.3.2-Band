a0b49b11a7fc15dae65b883b3b79bd20f23eaeea
David Chu <dchu101@thinkful.com>
2015-04-26 00:09:52 +0000

    Third commit

---
+++ test.py
@@ -0,0 +1,33 @@
+class Musician(object):
+    def __init__(self, sounds):
+        self.sounds = sounds
+
+    def solo(self, length):
+        for i in range(length):
+            print self.sounds[i % len(self.sounds)],
+        print ""
+
+class Bassist(Musician): # The Musician class is the parent of the Bassist class
+    def __init__(self):
+        # Call the __init__ method of the parent class
+        super(Bassist, self).__init__(["Twang", "Thrumb", "Bling"])
+
+class Guitarist(Musician):
+    def __init__(self):
+        # Call the __init__ method of the parent class
+        super(Guitarist, self).__init__(["Boink", "Bow", "Boom"])
+
+    def tune(self):
+        print "Be with you in a moment"
+        print "Twoning, sproing, splang"
+        
+class Drummer(Musician):
+    def __init__(self):
+        super(Drummer, self).__init__(["Crash", "Clink", "Pow"])
+        
+    def count(self):
+        for i in range(1, 5):
+            print i
+            
+    def combust(self):
+        print("Spontaneous combustion!")
